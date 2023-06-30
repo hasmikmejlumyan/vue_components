78981061a7265fe4517d753b462b6ac5d82e1440
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-06-30 09:34:12 +0400

    [Lesson-6] created components

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
+            // detailsAreVisible: false,
             friends: [
                 {
                     id: 'manuel',
@@ -19,11 +19,43 @@ const app = Vue.createApp({
         };
     },
 
+    // methods: {
+    //     toggleDetails() {
+    //         this.detailsAreVisible = !this.detailsAreVisible;
+    //     }
+    // }
+});
+
+app.component('friend-contact', {
+    template: `
+    <li>
+        <h2>{{ friend.name  }}</h2>
+        <button @click="toggleDetails">
+            {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+        </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>`,
+
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '1221212121',
+                email: 'manuel@lorenz.com',
+            },
+        };
+    },
+
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
         }
-    }
+    },
 });
 
 app.mount('#app');
